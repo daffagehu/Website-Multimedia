493c98851c3cd58d02ce0d57d6b2a46e0a3546ea
daffagehu <161928726+daffagehu@users.noreply.github.com>
2024-05-27 00:14:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Route utama
+// Mengatur folder public sebagai folder statis
+app.use(express.static(path.join(__dirname, 'Website')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'Website', 'index.html'));
 });
 
 // Menjalankan server
